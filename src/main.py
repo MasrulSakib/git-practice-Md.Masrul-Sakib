935fc880d6f9a58cbff4eca214c403040784c1f4
MasrulSakib <masrul.sakib69@gmail.com>
2026-05-01 08:29:08 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from datetime import date
+from utils import add, subtract
 
 print("My name is Md. Masrul Sakib")
 print("Today's date is", date.today())
+print("The sum of 5 and 3 is:", add(5, 3))
+print("The difference between 10 and 4 is:", subtract(10, 4))
