bdbda6e7b3ff5ba04792ae2423899b69b45f3c00
MasrulSakib <masrul.sakib69@gmail.com>
2026-05-01 08:39:43 +0600

    Add one more function (multiply)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from datetime import date
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("My name is Md. Masrul Sakib")
 print("Today's date is", date.today())
 print("The sum of 5 and 3 is:", add(5, 3))
 print("The difference between 10 and 4 is:", subtract(10, 4))
+print("The product of 6 and 7 is:", multiply(6, 7))
